ed3469ee7a4e958765bc2896d49f9a1e97648d5e
shubhamkaradegit <infoshubham1196@gmail.com>
2024-10-24 23:36:00 +0530

    formating

--- functions.c
+++ functions.c
@@ -1,64 +1,70 @@
-#include<stdio.h>
-#include<stdlib.h>
- int op;
-int num1,num2,ans;
-void addition(int,int);
-void subtraction(int,int);
-void multiplication(int,int);
-void division(int,int);
-int main(){
- 
-printf("Enter first number  : ");
-scanf("%d",&num1);
+#include <stdio.h>
+#include <stdlib.h>
+int op;
+int num1, num2, ans;
+void addition(int, int);
+void subtraction(int, int);
+void multiplication(int, int);
+void division(int, int);
+int main()
+{
 
-printf("Enter second number : ");
-scanf("%d",&num2);
+    printf("Enter first number  : ");
+    scanf("%d", &num1);
 
-printf("Enter the operation to perform (1 - '+',2 - '-',3 - '*',4 - '/'): ");
-scanf("%d",&op);
+    printf("Enter second number : ");
+    scanf("%d", &num2);
 
-switch(op){
-    case 1 : 
-        addition(num1,num2);
+    printf("Enter the operation to perform (1 - '+',2 - '-',3 - '*',4 - '/'): ");
+    scanf("%d", &op);
+
+    switch (op)
+    {
+    case 1:
+        addition(num1, num2);
         break;
 
-    case 2 : 
-        subtraction(num1,num2);
+    case 2:
+        subtraction(num1, num2);
         break;
 
-
-    case 3 : 
-        multiplication(num1,num2);
-        break;
-    
-    case 4 : 
-        division(num1,num2);
+    case 3:
+        multiplication(num1, num2);
         break;
 
-    default :
-       printf("\nEnter  a valid operation\n ");
-       break;
-}
+    case 4:
+        division(num1, num2);
+        break;
 
-return 0;
-}
-void addition(int x,int y){
-    ans=x+y;
-    printf("%d",ans);
-}
-void subtraction(int x,int y){
-    ans=x-y;
-    printf("%d",ans);
-}
-void multiplication(int x ,int y){
-    ans=x*y;
-    printf("%d",ans);
-}
-void division(int x,int y){
-    while(y==0){
-        printf("\nDivisor cannot be zero.Please enter anotther value :");
-        scanf("%d",&y);
+    default:
+        printf("\nEnter  a valid operation\n ");
+        break;
     }
-    ans=x/y;
-    printf("%d",ans);
+
+    return 0;
+}
+void addition(int x, int y)
+{
+    ans = x + y;
+    printf("%d", ans);
+}
+void subtraction(int x, int y)
+{
+    ans = x - y;
+    printf("%d", ans);
+}
+void multiplication(int x, int y)
+{
+    ans = x * y;
+    printf("%d", ans);
+}
+void division(int x, int y)
+{
+    while (y == 0)
+    {
+        printf("\nDivisor cannot be zero.Please enter anotther value :");
+        scanf("%d", &y);
+    }
+    ans = x / y;
+    printf("%d", ans);
 }
--- switch_algebra.c
+++ switch_algebra.c
@@ -1,44 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-int num1,num2,sum,diff,product,division,op;
+int main()
+{
+    int num1, num2, sum, diff, product, division, op;
 
-printf("Enter first number  : ");
-scanf("%d",&num1);
+    printf("Enter first number  : ");
+    scanf("%d", &num1);
 
-printf("Enter second number : ");
-scanf("%d",&num2);
+    printf("Enter second number : ");
+    scanf("%d", &num2);
 
-printf("Enter the number for operation (1 - '+',2 - '-',3 - '*',4 - '/'): ");
-scanf("%d",&op);
-switch(op){
+    printf("Enter the number for operation (1 - '+',2 - '-',3 - '*',4 - '/'): ");
+    scanf("%d", &op);
+    switch (op)
+    {
 
-    case 1 :
-       sum = num1 + num2;
-       printf("%d\n",sum);
-       break;
+    case 1:
+        sum = num1 + num2;
+        printf("%d\n", sum);
+        break;
 
-           case 2 :
-       diff = num1 - num2;
-       printf("%d\n",diff);
-       break;
-       
-           case 3 :
-       product = num1 * num2;
-       printf("%d\n",product);
-       break;
-       
-           case 4 :
-       division = num1 / num2;
-       if(num2 == 0){
-        printf("Divisor cannot be zero !!");
-       }
-       printf("%d\n",division);
-       break;
-       
-       default : 
-       printf("Not a valid operation!!!");
-}
+    case 2:
+        diff = num1 - num2;
+        printf("%d\n", diff);
+        break;
 
-return 0;
+    case 3:
+        product = num1 * num2;
+        printf("%d\n", product);
+        break;
+
+    case 4:
+        division = num1 / num2;
+        if (num2 == 0)
+        {
+            printf("Divisor cannot be zero !!");
+        }
+        printf("%d\n", division);
+        break;
+
+    default:
+        printf("Not a valid operation!!!");
+    }
+
+    return 0;
 }
